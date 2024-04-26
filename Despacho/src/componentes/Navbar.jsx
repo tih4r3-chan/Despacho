29643e210df6a60fc3374011400c6beb527eba45
not_kid0o <131726186+tih4r3-chan@users.noreply.github.com>
2024-04-26 18:46:51 -0400

    new
    cada vez peor, no me salenada como quiero :(

--- Despacho/src/componentes/Navbar.jsx
+++ Despacho/src/componentes/Navbar.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { RiUserFill } from 'react-icons/ri';
 
 function Navbar() {
   return (
-    <>
-    <nav className='bg-gray-800 p-4'>
-      <div className='hidden sm:ml-6 sm:block'>
+    <div className=''>
+    <nav className='bg-gray-800 p-4 flex items-center justify-between'>
+      <div className='flex items-centser'>
+        {/* nombre/ logo */}
         <div>
           <p className='text-white'>Despacho</p>
         </div>
+        {/* opciones */}
         <div className='items-center space-x-4'>
-          <a href="#" className='text-white'>Seguimiento</a>
-          <a href="#" className='text-white'>Sucursal</a>
-          <a href="#" className='text-white'>Contato</a>
+          <a href="#" className='text-white ml-4'>Seguimiento</a>
+          <a href="#" className='text-white ml-4'>Sucursal</a>
+          <a href="#" className='text-white ml-4'>Contato</a>
+        </div>
+        {/* botonperfin */}
+        <div className='hidden sm:block x-end'>
+          <RiUserFill className="h-8 w-8 text-white" />
         </div>
       </div>
     </nav>
-    </>
+    </div>
   );
 }
 
