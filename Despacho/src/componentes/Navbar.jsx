67b51792ac17924581e11badf9437a51ccdbe48c
not_kid0o <131726186+tih4r3-chan@users.noreply.github.com>
2024-04-26 14:54:36 -0400

    new
    trabajando la barra de navegacion

--- Despacho/src/componentes/Navbar.jsx
+++ Despacho/src/componentes/Navbar.jsx
@@ -3,9 +3,18 @@ import React from 'react';
 function Navbar() {
   return (
     <>
-    <div className='bg-blue-900'>
-      <h1>aaaaa</h1>
-    </div>
+    <nav className='bg-gray-800 p-4'>
+      <div className='hidden sm:ml-6 sm:block'>
+        <div>
+          <p className='text-white'>Despacho</p>
+        </div>
+        <div className='items-center space-x-4'>
+          <a href="#" className='text-white'>Seguimiento</a>
+          <a href="#" className='text-white'>Sucursal</a>
+          <a href="#" className='text-white'>Contato</a>
+        </div>
+      </div>
+    </nav>
     </>
   );
 }
