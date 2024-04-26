7ab50d97ecd03f1c9d66f5c3ee7125069c608772
not_kid0o <131726186+tih4r3-chan@users.noreply.github.com>
2024-04-26 14:31:02 -0400

    new
    ahorasiiiiii

--- Despacho/src/App.jsx
+++ Despacho/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-      <div className=''>
-        <Navbar />
+      <div>
+        <Navbar/>
       </div>
     </>
   )
--- Despacho/src/componentes/Navbar.jsx
+++ Despacho/src/componentes/Navbar.jsx
@@ -3,22 +3,9 @@ import React from 'react';
 function Navbar() {
   return (
     <>
-        <nav className=''>
-            <div className=''>
-                <p>Logo</p>
-            </div>
-            <div className=''>
-              <a href="#" className=''>
-                Seguimiento
-              </a>
-              <a href="#" className=''>
-                Sucursal
-              </a>
-              <a href="#" className=''>
-                Contacto
-              </a>
-            </div>
-        </nav>
+    <div className='bg-blue-900'>
+      <h1>aaaaa</h1>
+    </div>
     </>
   );
 }
