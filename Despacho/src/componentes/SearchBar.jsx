a5d27c44f69eadb8e8afccce2f4876fb03755420
not_kid0o <131726186+tih4r3-chan@users.noreply.github.com>
2024-04-30 09:15:04 -0400

    new
    aaaaa

--- Despacho/src/componentes/SearchBar.jsx
+++ Despacho/src/componentes/SearchBar.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import LogoSearchBar from "../assets/images/logistics-delivery.png"
-
 export const SearchBar = () => {
   return (
     <>
         <section className="grid grid-cols-12">
             <div className="grid-span-4">
-                <img src={LogoSearchBar} alt="logo" />
+                <img src={''} alt="logo" />
             </div>
             <div className="grid-span-8">
                 holax2
