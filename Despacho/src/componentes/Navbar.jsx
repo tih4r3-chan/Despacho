4da89c665ef70b7640f726eeb058255daefc2b9d
“PsTam27“ <pi.sat@duocuc.cl>
2024-05-06 23:39:19 -0400

    aaaa

--- Despacho/src/componentes/Navbar.jsx
+++ Despacho/src/componentes/Navbar.jsx
@@ -4,12 +4,13 @@ function Navbar() {
   return (
     <nav className="bg-teal-500 border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="" className="h-8" alt="acavaellogo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tiharefunada</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar Sesión</button>
+          <button type="button" className="text-white  text-gray-900  md:dark:hover:text-gray-800  f
+           focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center ">Iniciar Sesión</button>
           <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
