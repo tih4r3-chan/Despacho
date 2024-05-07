4e2c6d0d74aec9724a509e06ea7e0cde25dd611f
reaien <ivanduarteherrera@gmail.com>
2024-05-07 09:19:57 -0400

    little changes

--- Despacho/src/componentes/Navbar.jsx
+++ Despacho/src/componentes/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="" className="h-8" alt="acavaellogo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Tiharefunada
+            Tihare & Pia Funadas S.A
           </span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
