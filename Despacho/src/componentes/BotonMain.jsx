b6d427b2d26cfa1ee37ef33ebeb3e957ac569d98
“PsTam27“ <pi.sat@duocuc.cl>
2024-05-06 22:39:12 -0400

    navbar

--- Despacho/src/componentes/BotonMain.jsx
+++ Despacho/src/componentes/BotonMain.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 function BotonMain({className }) {
   return (
-    <button
-      className={`"px-16 bg-teal-500/80 border rounded-xl font-bold hover:bg-teal-600 transition-all duration-500 text-white">${className}`}>
-      ayuda xfavorx
-    </button>
+<button
+  className={`px-8 py-4 bg-teal-500/80 border rounded-xl font-bold hover:bg-teal-600 transition-all duration-500 text-white ${className}`}
+>ayuda xfavor
+</button>
+
   );
 }
 
