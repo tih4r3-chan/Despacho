9e6802b0c555157d1a1881a706bbd59cf4e64ba0
Reaien <ivanduarteherrera@gmail.com>
2024-05-12 18:27:44 -0400

    table with endpoint data

--- Despacho/src/componentes/TableCompras.jsx
+++ Despacho/src/componentes/TableCompras.jsx
@@ -1,40 +1,53 @@
+import { useState, useEffect } from "react";
 import React from "react";
 
 export const TableCompras = () => {
+  const [ventas, setVentas] = useState([]);
+
+  const compras = async () => {
+    const response = await fetch(
+      "https://my-json-server.typicode.com/tih4r3-chan/Despacho/Ventas"
+    );
+    const data = await response.json();
+    console.log(data);
+    setVentas(data);
+  };
+  // Llamada a la función para obtener los datos cuando el componente se monta
+  useEffect(() => {
+    compras();
+  }, []);
+
   return (
     <>
-      <section className="grid text-center grid-cols-12 mb-[323px]">
+      <section className="grid text-center grid-cols-12 mb-8">
         <div className="col-span-12 flex justify-center">
           <div className="col-span-10 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-white h-full overflow-hidden">
             <table className="table-fixed">
               <thead>
-                <tr>
+                <tr className="py-10">
                   <th className="pr-10">n° de compra</th>
                   <th className="pr-10">direccion</th>
                   <th className="pr-10">fecha de compra</th>
                   <th className="pr-10">valor total</th>
-                  <th className="pr-10">Intento de despachos</th>
-                  <th className="pr-10">Entregado</th>
-                  <th className="pr-10">Cerrado</th>
+                  <th className="pr-10"></th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td className="pr-10">1</td>
-                  <td className="pr-10">Avenida siempre viva 69</td>
-                  <td className="pr-10">09-04-2024</td>
-                  <td className="pr-10">$10.990</td>
-                  <td className="pr-10">2</td>
-                  <td className="pr-10">No</td>
-                  <td className="pr-10">No</td>
-                </tr>
+                {ventas.map((venta) => (
+                  <tr key={venta.id} className="py-12">
+                    <td className="pr-10">{venta.id}</td>
+                    <td className="pr-10">{venta.direccion_compra}</td>
+                    <td className="pr-10">{venta.fecha_compra}</td>
+                    <td className="pr-10">${venta.valor_compra}</td>
+                    <td>
+                      <button className="py-1 bg-orange-200 px-8 rounded-xl shadow-md hover:bg-orange-300/70 transition-all duration-300">
+                        Generar Despacho
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
-            <div className="flex justify-center mt-4">
-              <button className="py-1 bg-orange-200 px-8 rounded-xl shadow-md hover:bg-orange-300/70 transition-all duration-300">
-                Editar
-              </button>
-            </div>
           </div>
         </div>
       </section>
