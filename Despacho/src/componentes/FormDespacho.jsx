17b212a09cfbbc1bd610ddda161e73921c6fb463
Reaien <ivanduarteherrera@gmail.com>
2024-05-18 14:34:23 -0400

    "olvide commitear esto"

--- Despacho/src/componentes/FormDespacho.jsx
+++ Despacho/src/componentes/FormDespacho.jsx
@@ -21,7 +21,7 @@ export const FormDespacho = ({ venta, onClose }) => {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/despachos/",
+        "http://44.205.221.190:8000/despachos/",
         jsonData,
         {
           headers: {
