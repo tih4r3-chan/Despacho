2546d831901ecb6df49a8948422ab978a1116a2f
“PsTam27“ <pi.sat@duocuc.cl>
2024-06-11 17:13:56 -0400

    modal login

--- Despacho/src/componentes/ModalLogin.jsx
+++ Despacho/src/componentes/ModalLogin.jsx
@@ -1,47 +1,35 @@
 import React from "react";
+import logo1 from '../assets/images/logo2.png';
 
 function ModalLogin({ open, onClose }) {
   return (
     <div className={`fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 ${open ? "" : "hidden"}`}>
-      <div className="bg-white p-8 rounded-lg">
-        <h2 className="text-2xl font-bold mb-4">Iniciar Sesión</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Usuario
-            </label>
-            <input
-              type="text"
-              id="username"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Ingrese su usuario"
-            />
+      <div className="bg-white p-12 w-80 rounded-lg relative">
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-4 text-teal-500/80 hover:text-teal-600 font-bold text-3xl"
+        >
+          &times;
+        </button>
+        <div className="flex justify-center mb-4">
+          <img src={logo1} alt="Logo" className="w-20 h-20" />
+        </div>
+        <h2 className="text-center text2xl font-bold mb-4">Inicio de Sesión</h2>
+        <form className="max-w-sm mx-auto">
+          <div className="mb-5"> 
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Tu email</label>
+            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-300 dark:border-gray-400 dark:placeholder-gray-400 dark:text-white dark:shadow-sm-light" placeholder="aaa@aaa.com" required />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Contraseña
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Ingrese su contraseña"
-            />
+          <div className="mb-5">
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Tu contraseña</label>
+            <input type="password" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-300 dark:border-gray-400 dark:placeholder-gray-400 dark:text-white dark:shadow-sm-light" required />
+          </div>
+
+          <div className="flex items-start mb-5">
+            <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-700 dark:text-gray-400">No tienes cuenta? <a href="#" className="text-teal-600 hover:text-teal-800 hover:underline">Registrate</a></label>
           </div>
           <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Iniciar Sesión
-            </button>
-            <button
-              type="button"
-              onClick={onClose}
-              className="inline-block align-baseline font-bold text-sm text-teal-600 hover:text-teal-800"
-            >
-              Cerrar
-            </button>
+            <button type="submit" className="text-white focus:outline-none font-medium r text-sm px-5 py-2.5 text-center bg-teal-500/80 hover:bg-teal-600 transition-all duration-500 flex w-full justify-center rounded-md">Iniciar sesión</button>
           </div>
         </form>
       </div>
