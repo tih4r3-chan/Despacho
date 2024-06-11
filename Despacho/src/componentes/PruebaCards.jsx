e2fe792b70862f55e15f61978eab6f93eaeb78ab
“PsTam27“ <pi.sat@duocuc.cl>
2024-06-11 08:35:08 -0400

    a

--- Despacho/src/componentes/PruebaCards.jsx
+++ Despacho/src/componentes/PruebaCards.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardComponent } from "./CardComponent";
+import { TableCompras } from "./TableCompras";
 
 export const PruebaCards = () => {
+  const [tablaCompras, setTablaCompras] = useState(false);
+  const [tablaOrdenes, setTablaOrdenes] = useState(false);
+
   return (
-    <div className="flex justify-center space-x-7">
-      <CardComponent
-        title="Consultar Ordenes de compra"
-        description="Revisa las últimas oc realizadas para generar su despacho"
-        buttonText="Consultar"
-      />
-      <CardComponent
-        title="Revisar Ordenes de despacho"
-        description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
-        buttonText="Consultar"
-      />
-      <CardComponent
-        title="Sucursales"
-        description="Conoce las agencia más cercanas"
-        buttonText="Sucursales"
-      />
-    </div>
+    <section>
+      <div className="flex justify-center space-x-7">
+        <CardComponent
+          title="Consultar Ordenes de compra"
+          description="Revisa las últimas oc realizadas para generar su despacho"
+          buttonText="Consultar"
+          onClick={() => {
+            setTablaCompras(true);
+            setTablaOrdenes(false);
+          }}
+        />
+        <CardComponent
+          title="Revisar Ordenes de despacho"
+          description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
+          buttonText="Consultar"
+          onClick={() => {
+            setTablaCompras(false);
+            setTablaOrdenes(true);
+          }}
+        />
+        <CardComponent
+          title="Sucursales"
+          description="Conoce las agencia más cercanas"
+          buttonText="Sucursales"
+        />
+      </div>
+
+      <section>
+        {tablaCompras && <TableCompras />}
+        {tablaOrdenes && <div>hola</div>}
+      </section>
+    </section>
   );
 };
